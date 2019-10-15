daa9f54d2b994eaa2fce5832d26319436b2aaa0f
miguel.ccader@gmail.com <miguel.ccader@gmail.com>
2019-10-15 12:05:29 -0600

    Adding files to the new repository

---
+++ main.js
@@ -0,0 +1,51 @@
+var pesta = document.querySelectorAll(".blog a");
+var week = document.querySelectorAll(".day");
+var show = week[0];
+
+window.onload = function() {
+  console.log(show);
+  for (let i = 0; i < 5; i++) {
+    pesta[i].addEventListener("click", () => {
+      show.classList.add("hidden");
+      show = week[i];
+      show.classList.remove("hidden");
+      console.log(show);
+    });
+  }
+
+  /*
+  pesta[0].addEventListener("click", () => {
+    show.classList.add("hidden");
+    show = week[0];
+    show.classList.remove("hidden");
+    console.log(show);
+  });
+  pesta[1].addEventListener("click", () => {
+    show.classList.add("hidden");
+    show = week[1];
+    show.classList.remove("hidden");
+    console.log(show);
+  });
+
+  pesta[2].addEventListener("click", () => {
+    show.classList.add("hidden");
+    show = week[2];
+    show.classList.remove("hidden");
+    console.log(show);
+  });
+
+  pesta[3].addEventListener("click", () => {
+    show.classList.add("hidden");
+    show = week[3];
+    show.classList.remove("hidden");
+    console.log(show);
+  });
+
+  pesta[4].addEventListener("click", () => {
+    show.classList.add("hidden");
+    show = week[4];
+    show.classList.remove("hidden");
+    console.log(show);
+  });
+*/
+};
