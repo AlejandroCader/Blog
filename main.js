8927264bb2f7c455845250dcc7c96fa8675271b2
miguel.ccader@gmail.com <miguel.ccader@gmail.com>
2019-10-15 18:39:31 -0600

    Adding basic changes to the JS file

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var show = week[0];
 
 window.onload = function() {
   console.log(show);
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i <= 4; i++) {
     pesta[i].addEventListener("click", () => {
       show.classList.add("hidden");
       show = week[i];
